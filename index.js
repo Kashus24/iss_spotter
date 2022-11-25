b5a38f97b122b0990147cd9c54f551fd69bc16d3
Akashdeep Sekhon <kash@Akashdeeps-MacBook-Pro.local>
2022-11-24 21:22:58 -0500

    index example tests

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
+const { fetchMyIP } = require('./iss');
+const { fetchCoordsByIP } = require('./iss');
+const { fetchISSFlyOverTimes } = require('./iss');
 
+
+
+
+
+const exampleCoords = { lat: '43.7315479', lon: '-79.7624177' };
+
+fetchISSFlyOverTimes(exampleCoords, (error, passTimes) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
+
+  console.log('It worked! Returned flyover times:' , passTimes);
+});
+
+
+
+
+// fetchCoordsByIP('99.234.196.45', (error, data) => {
+//   if (error) {
+//     console.log('It didn\'t work!', error);
+//     return;
+//   }
+//   console.log('It worked! Returned data: ', data);
+// });
+
+
+
+// fetchMyIP((error, ip) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
+
+//   console.log('It worked! Returned IP:' , ip);
+// });
